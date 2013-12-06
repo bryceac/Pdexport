cc6e747a90e06928785dde79324fb0a8837ce2dd
Bryce <tonyhawk2100@gmail.com>
2013-12-06 09:30:36 -0800

    start work on plugin

--- pdfexport.plugin.php
+++ pdfexport.plugin.php
@@ -9,16 +9,42 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
+require_once('dompdf/dompdf_config.inc.php'); // import DOMPDF
+
 // the PDexport class is used to export PDFs
 class PDexport extends Plugin
 {
-	private $feed;
+	private $dompdf;
 	
-	/* public function action_init()
+	public function action_init()
 	{
-		
-	} */
+		$this->dompdf = new DOMPDF(); // initialize DOMPDF object
+	}
 	
+	public function filter_post_content_out($content, $post)
+	{
+		$content = $content; // setup variable to set link location
+		return content;
+	}
+	
+	public function pdf_content($theme, $post)
+	{
+		return $this->create_pdf($post); // send post content to PDF
+	}
+	
+	public function create_pdf($post)
+	{
+		$this->dompdf->load_html($post->content_out); // load html contents
+		$this->dompdf->render(); // render content
+		$this->stream($post->title . '.pdf'); // set filename
+		
+	}
+	
+	public function pdf_link()
+	{
+		$link = '<a href="">Save as PDF</a>';
+		return link;
+	}
 	// the following sets default values upon activation
 	/* public function action_plugin_activation($file)
 	{
